edb21286e9375f4c5e229f6b6a390f2a36a19176
Supriya <makeinnovative.00@gmail.com>
2023-07-08 17:44:05 +0530

    Second commit

--- ecommerceapp/src/App.js
+++ ecommerceapp/src/App.js
@@ -1,11 +1,28 @@
+import React  from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import About from "./About";
+import Contact from "./Contact";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import Cart from './Cart';
+import ErrorPage from './ErrorPage';
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      hello world
-    </div>
-  );
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+};
 
 export default App;
